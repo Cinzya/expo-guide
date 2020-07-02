afe9e40db1ced406ce419e453b39a63210921200
Cinzya <cynti-ebert@web.de>
2020-07-02 03:19:08 +0200

    created empty navigation component

---
+++ components/navigation.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default function Navigation() {
+    return (
+        <View style={styles.container}>
+            
+        </View>
+    )
+}
