82c389016cbfd4b55475eb20a250d3f9f33de97c
Cinzya <cynti-ebert@web.de>
2020-07-02 04:03:58 +0200

    created stack navigation

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import HomeScreen from './screens/home';
+import HomeScreen from './screens/HomeScreen';
+import StackNavigator from './navigation/stackNavigator';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeScreen/>
+      <StackNavigator />
     </View>
   );
 }
