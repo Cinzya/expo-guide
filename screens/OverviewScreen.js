0c8cf4a2ed8d999322b1bf5d93697557a78e32cc
Cinzya <cynti-ebert@web.de>
2020-08-12 14:44:52 +0200

    added scrollview

--- screens/OverviewScreen.js
+++ screens/OverviewScreen.js
@@ -4,28 +4,32 @@ import { Icon } from 'react-native-elements';
 
 
 import { styles } from '../styles/styles';
-import { TouchableNativeFeedback } from 'react-native-gesture-handler';
+import { TouchableNativeFeedback, ScrollView } from 'react-native-gesture-handler';
 
 export default function OverviewScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.h1}>Übersicht Ausstellungsstücke</Text>
 
-      <TouchableNativeFeedback
-      onPress={() => navigation.navigate('Exhibit')}>
-      <View style={styles.exhibit}>
-        <Text style={styles.exhibitText}>Name des Ausstellungsstücks</Text>
-        <Icon name='arrow-right' type='material-community' color='#D36462' reverse/>
+      <ScrollView>
+        <TouchableNativeFeedback
+          onPress={() => navigation.navigate('Exhibit')}>
 
-      </View>
-      </TouchableNativeFeedback>
+          <View style={styles.exhibit}>
+            <Text style={styles.exhibitText}>Name des Ausstellungsstücks</Text>
+            <Icon name='arrow-right' type='material-community' color='#D36462' reverse/>
 
-      <TouchableNativeFeedback
-      onPress={() => navigation.navigate('Exhibit')}>
-      <View style={styles.exhibit}>
-        <Text style={styles.exhibitText}>Name des Ausstellungsstücks</Text>
-      </View>
-      </TouchableNativeFeedback>
+          </View>
+        </TouchableNativeFeedback>
+
+        <TouchableNativeFeedback
+          onPress={() => navigation.navigate('Exhibit')}>
+            
+          <View style={styles.exhibit}>
+            <Text style={styles.exhibitText}>Name des Ausstellungsstücks</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </ScrollView>
     </View>
   );
 }
